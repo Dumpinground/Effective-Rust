2bb4fcc07245611ba85ab017e749f307af06f1a5
Ad <DeadLimit@lkr5.onmicrosoft.com>
2023-06-09 11:22:29 +0800

    update

--- examples/Item2/main.rs
+++ examples/Item2/main.rs
@@ -31,3 +31,44 @@ fn whats_the_area() {
     println!("r: {}", r.area());
     println!("c: {}", c.area());
 }
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+#[test]
+fn function_pointer() {
+    let op: fn(i32, i32) -> i32 = sum;
+    println!("{}", op(1, 2));
+
+    let op1 = op;
+    let op2 = op;
+    assert!(op1 == op2);
+    println!("op = {:p}", op);
+}
+
+pub fn modify_all(data: &mut [u32], mutator: fn(u32) -> u32) {
+    for value in data {
+        *value = mutator(*value);
+    }
+}
+
+fn add2(v: u32) -> u32 {
+    v + 2
+}
+
+#[test]
+fn modify_data_by_2() {
+    let mut data = vec![1, 2, 3];
+    modify_all(&mut data, add2);
+    assert_eq!(data, vec![3, 4, 5]);
+    println!("{:?}", data);
+}
+
+#[test]
+fn add_by_n() {
+    let amount_to_add = 3;
+    let add_n = |y| y + amount_to_add;
+    let z = add_n(5);
+    assert_eq!(z, 8);
+}
--- test.nu
+++ test.nu
@@ -1,8 +1,9 @@
 #!/usr/bin/env nu
 
+# test a example
 def main [
-  id: int
-  name: string
+  id: int # Input example item id. Test in Item1 when id = 1.
+  name = '' # Input test function name. Default to show all tests.
 ] {
   cargo test --example ('Item' + ($id | into string) ) $name -- --nocapture
 }
